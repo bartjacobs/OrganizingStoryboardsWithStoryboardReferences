3a430286f10b8305d2ad8f6e9f7ea3a0c4d836fc
Bart Jacobs <developer@bartjacobs.com>
2016-07-20 19:21:11 +0200

    Add ability to edit notes

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AddNoteViewControllerDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate, AddNoteViewControllerDelegate {
 
     @IBOutlet var tableView: UITableView!
     
@@ -20,12 +20,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let fetchRequest = NSFetchRequest(entityName: "Note")
 
         // Add Sort Descriptors
-        let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
+        let sortDescriptor = NSSortDescriptor(key: "updatedAt", ascending: false)
         fetchRequest.sortDescriptors = [sortDescriptor]
 
         // Initialize Fetched Results Controller
         let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.coreDataManager.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
 
+        // Set Delegate
+        fetchedResultsController.delegate = self
+        
         return fetchedResultsController
     }()
 
@@ -52,6 +55,53 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 // Configure View Controller
                 viewController.delegate = self
             }
+        } else if segue.identifier == "SegueNoteViewController" {
+            if let viewController = segue.destinationViewController as? NoteViewController {
+                if let indexPath = tableView.indexPathForSelectedRow, let note = fetchedResultsController.objectAtIndexPath(indexPath) as? Note {
+                    // Configure View Controller
+                    viewController.note = note
+                }
+            }
+        }
+    }
+
+
+    // MARK: - Fetched Results Controller Delegate Methods
+
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        tableView.beginUpdates()
+    }
+
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        tableView.endUpdates()
+    }
+
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        switch (type) {
+        case .Insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            }
+            break;
+        case .Delete:
+            if let indexPath = indexPath {
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            }
+            break;
+        case .Update:
+            if let indexPath = indexPath, let cell = tableView.cellForRowAtIndexPath(indexPath) {
+                configureCell(cell, atIndexPath: indexPath)
+            }
+            break;
+        case .Move:
+            if let indexPath = indexPath {
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            }
+
+            if let newIndexPath = newIndexPath {
+                tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Fade)
+            }
+            break;
         }
     }
 
@@ -69,13 +119,32 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("NoteCell", forIndexPath: indexPath)
 
+        // Configure Cell
+        configureCell(cell, atIndexPath: indexPath)
+
+        return cell
+    }
+
+    func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
         // Fetch Note
         if let note = fetchedResultsController.objectAtIndexPath(indexPath) as? Note {
             cell.textLabel?.text = note.title
             cell.detailTextLabel?.text = note.content
         }
-        
-        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            if let note = fetchedResultsController.objectAtIndexPath(indexPath) as? Note {
+                fetchedResultsController.managedObjectContext.deleteObject(note)
+            }
+        }
+    }
+
+    // MARK: - Table View Delegate Methods
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 
     // MARK: - Add Note View Controller Delegate Methods
